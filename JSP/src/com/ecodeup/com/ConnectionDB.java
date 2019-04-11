fc0dd7da50bd0a2dde2d22d384d425b0afb5af21
Hongzz <cf17hong.zhon@iesjoandaustria.org>
2019-04-11 20:15:04 +0200

    ConnectionDB.java: add a password with a parameter

--- JSP/src/com/ecodeup/com/ConnectionDB.java
+++ JSP/src/com/ecodeup/com/ConnectionDB.java
@@ -9,10 +9,11 @@ public class ConnectionDB {
 	static String URL="jdbc:hsqldb:C:\\Users\\cf17h\\eclipse-workspace\\JSP\\WebContent\\WEB-INF\\lib\\srv_db.tmp";
 	public static Connection getConnection() {
 		Connection conn = null;
+		String ps="";
 		try {
 			LoginCase.select(DRIVER);
 			Class.forName(DRIVER);
-			conn=DriverManager.getConnection(URL,"sa", "hong");
+			conn=DriverManager.getConnection(URL,"sa", ps);
 		}catch(ClassNotFoundException | SQLException e) {
 			System.err.println("Error: "+e);
 		}
