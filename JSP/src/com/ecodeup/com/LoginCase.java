f8acd8549ac865ea76944fd0c485ea2b4d72c306
Hongzz <cf17hong.zhon@iesjoandaustria.org>
2019-05-25 10:15:39 +0200

    LoginCase.java: add javadoc comments

--- JSP/src/com/ecodeup/com/LoginCase.java
+++ JSP/src/com/ecodeup/com/LoginCase.java
@@ -7,21 +7,34 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.logging.Level; 
 import java.util.logging.Logger;
-
-//este puto java es para guardar las funciones que vamos a utilizar despues
+/**+
+ * esta clase permite  definir metodo
+ * para acceder a la bbdd i hacer un insert 
+ * @author cf17h
+ *
+ */
 public class LoginCase {
-	
+	final static Logger LOGGER = Logger.getLogger("com.ecodeup.com");//variable para hacer print
+	/**
+	 * Constructor por defento
+	 */
 	private LoginCase() { }
-	//funcion para insertar
-	//parametros de entrada name pass email, (los introducidos por html de login) 
+	/**
+	 * metodo que permite hacer un insert de user
+	 * @param uName - valor nombre ingresado por el usuario
+	 * @param uPass - valor password ingresado por el usuario
+	 * @param uEmail - valor email ingresado por el usuario
+	 * @return retorna un valor boolean, para ver si inserto o no
+	 */
 	public static boolean insertUser(String uName, String uPass, String uEmail) {
+		//definimos los varibles que necesitaremos para conectar a bbdd
 		Connection con;
-		
 		con=ConnectionDB.getConnection();
 		PreparedStatement ps =null;
 		
+		
 		try {
-			//la tabla que habiamos creado antes de hacer el insert
+			// la tabla de bbdd
 			String bbdd = "CREATE TABLE USER (\r\n" + 
 					" ID varchar(6) PRIMARY KEY,\r\n" + 
 					" NICK varchar(20) NOT NULL, \r\n" + 
@@ -29,12 +42,14 @@ public class LoginCase {
 					" PASSWORD varchar(20) NOT NULL,\r\n" + 
 					");";
 			
+			//definimos la consulta del insert
 			String consulta = "insert into USER (NICK,EMAIL,PASSWORD) VALUES(?,?,?);";
 			ps=con.prepareStatement(consulta);
+			//cogemos los variables ingresado por el cliente
 			ps.setString(1, uName);
 			ps.setString(2, uPass);
 			ps.setString(3, uEmail);
-			//si todo va bien return true
+			//si todo va bien TRUE
 			if(ps.executeUpdate()==1) {
 				return true;
 			}
@@ -50,7 +65,7 @@ public class LoginCase {
 				e.printStackTrace();
 			}
 
-	}
+	}//SI no false
 		return false;
 }
 	
