d621527d05bb5a7f47970dc1b1618e5e83719758
Hongzz <cf17hong.zhon@iesjoandaustria.org>
2019-05-25 10:15:20 +0200

    tienda.java: add javadoc comments

--- JSP/src/com/ecodeup/com/tienda.java
+++ JSP/src/com/ecodeup/com/tienda.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,23 +15,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class nameeee
+ * Servlet implementation class tienda
+ * permite conectar entre paginas
+ * mediante POST o GET
+ *  
+ * @author cf17h
+ *
  */
+
+
 @WebServlet("/tienda")
 
 public class tienda extends HttpServlet {
-	//definimos las cosas que vamos a utilizar despues
 	private static final long serialVersionUID = 1L;
 	Properties prop = new Properties();
-	final static Logger LOGGER = Logger.getLogger("com.ecodeup.com");
 	InputStream is = null;   
     /**
+     * constructor defecto
      * @see HttpServlet#HttpServlet()
      */
 	private tienda() { }
-	
-	//para guardar las cosas mas seguramente vamos a utilizar POST
+
 	/**
+	 * metode con la ayuda del servlet request y reponse 
+	 * metode GET
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -42,35 +47,38 @@ public class tienda extends HttpServlet {
 	}
 
 	/**
+	 * metode con la ayuda del servlet request y reponse 
+	 * hara un metode POST , desde el formulario del insert
+	 * a una otra pagina dependiento de la informacion introducida
+	 * vera si estan en formato esperado
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//leemos ese fichero propertie para uzarlo despues
 		try {
+			//abrimos el fichero properties para utilizarlo despues
 			is = new FileInputStream("C:\\Users\\cf17h\\git\\Hongz\\JSP\\WebContent\\jsp\\help.properties");
 			prop.load(is);
 		} catch(IOException e) {
-			String msn = "la exception es ";
-	       	 LOGGER.log(Level.INFO,msn, e);
+			System.out.println(e.toString());
 		}
-		//guardamos los parametros obtenidos a una variable
+		// hacemos una pruba a ver si las informaciones introducida por el usuario
+		//corresponde al pattern de properties
+		
 		String nick= request.getParameter("nick");
 		
 		String select = request.getParameter("select");
 	
-		//hacemos el pattern para que compile bien lo que introduce el usuario, mediante el fichero properties obtenemos el pattern
+		
 		Pattern pat1 =Pattern.compile(prop.getProperty("p4"));
 		Matcher mat1=pat1.matcher(nick);
 		
 		Pattern pat2 =Pattern.compile(prop.getProperty("p5"));
 		Matcher mat2=pat2.matcher(select);
 
-		
-		//conjunto redirecciones de ifs, dependiendo del que introdusca el usuario ira a una otra jsp 	
-		
+		// si todo es correcto i sino ira dependiento el error
 		if (mat1.find()) {
 				if (mat2.find()) {
-					
+					//hara el insert i ira a la pagina validado		
 			response.sendRedirect("/JSP/jsp/validar.jsp");
 			
 			
