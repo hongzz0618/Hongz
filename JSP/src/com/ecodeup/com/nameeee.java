8fc31c74b1c8d26340ff08c1347289bbfbceb340
Hongzz <cf17hong.zhon@iesjoandaustria.org>
2019-05-25 10:14:18 +0200

    nameeee.java: add javadoc comments

--- JSP/src/com/ecodeup/com/nameeee.java
+++ JSP/src/com/ecodeup/com/nameeee.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,24 +16,27 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class nameeee
+ * permite conectar entre paginas
+ * mediante POST o GET
+ *  
+ * @author cf17h
+ *
  */
 @WebServlet("/nameeee")
 
 public class nameeee extends HttpServlet {
+	/**
+	 * constructor defecto
+	 */
 	private nameeee() { }
-	//definimos las cosas que vamos a utilizar despues
-	final static Logger LOGGER = Logger.getLogger("com.ecodeup.com");
+	//definimos los variables que vamos a utilizar
 	private static final long serialVersionUID = 1L;
 	Properties prop = new Properties();
 	InputStream is = null;
-	
-    /**
-     * 
-     * @see HttpServlet#HttpServlet()
-     */
-    
-//para guardar las cosas mas seguramente vamos a utilizar POST
+
 	/**
+	 * metode con la ayuda del servlet request y reponse 
+	 * metode GET
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -44,30 +45,42 @@ public class nameeee extends HttpServlet {
 	}
 
 	/**
+	 * metode con la ayuda del servlet request y reponse 
+	 * hara un metode POST , desde el formulario del insert
+	 * a una otra pagina dependiento de la informacion introducida
+	 * vera si estan en formato esperado
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//leemos ese fichero propertie para uzarlo despues
-		//guardamos los parametros obtenidos a una variable
+		try {
+			//abrimos el fichero properties para utilizarlo despues
+			is = new FileInputStream("C:\\Users\\cf17h\\git\\Hongz\\JSP\\WebContent\\jsp\\help.properties");
+			prop.load(is);
+		} catch(IOException e) {
+			System.out.println(e.toString());
+		}
+		// hacemos una pruba a ver si las informaciones introducida por el usuario
+		//corresponde al pattern de properties
 		String user = request.getParameter("user");
 		String password = request.getParameter("password");
 		String email = request.getParameter("email");
-		//hacemos el pattern para que compile bien lo que introduce el usuario, mediante el fichero properties obtenemos el pattern
-		Pattern pat =Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+		Pattern pat =Pattern.compile(prop.getProperty("p1"));
 		Matcher mat=pat.matcher(email);
-		Pattern pat1 =Pattern.compile("^[A-Za-z0-9]{1,10}$");
+		Pattern pat1 =Pattern.compile(prop.getProperty("p2"));
 		Matcher mat1=pat1.matcher(user);
-		Pattern pat2 =Pattern.compile("^[A-Za-z0-9]{8,}$");
+		Pattern pat2 =Pattern.compile(prop.getProperty("p3"));
 		Matcher mat2=pat2.matcher(password);
 
-	//conjunto redirecciones de ifs, dependiendo del que introdusca el usuario ira a una otra jsp 	
+		
+		// si todo es correcto i sino ira dependiento el error
 		if (mat1.find()) {
 			if (mat.find()) {
 				if (mat2.find()) {
-				
+				//hara el insert i ira a la pagina validado
 						LoginCase.insertUser(user,password,email);	
 						response.sendRedirect("/JSP/jsp/validar.jsp");
-		
+					
 			
 		}else {
 			response.sendRedirect("/JSP/jsp/validarno.jsp");
