7ff761b7a377762732f5748542d5e392bbe35cac
Hongzz <cf17hong.zhon@iesjoandaustria.org>
2019-05-25 10:16:17 +0200

    ConnectionDB.java: add javadoc comments

--- JSP/src/com/ecodeup/com/ConnectionDB.java
+++ JSP/src/com/ecodeup/com/ConnectionDB.java
@@ -5,25 +5,36 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level; 
 import java.util.logging.Logger; 
+/**
+ * Esta clase permite  definir metodo 
+ * donde se guarda las informaciones propies, para luego conectarse a la bbdd hsqldb
+ * @author cf17h
+ *
+ */
 public class ConnectionDB {
-	
-//este punto java es donde se guarda las informaciones propies, para luego conectarse a la bbdd hsqldb
 	static String DRIVER="org.hsqldb.jdbcDriver";
 	static String URL="jdbc:hsqldb:C:\\Users\\cf17h\\eclipse-workspace\\JSP\\WebContent\\WEB-INF\\lib\\srv_db.tmp";
-	//el driver y la Url, definimos lloguer para imprimir por pantalla
 	final static Logger LOGGER = Logger.getLogger("com.ecodeup.com");
+	
+	/**
+	 * Constructor por defento
+	 */
 	private ConnectionDB() { }
+	/**
+	 * metode que permite conectar a bbdd
+	 * mediante las informaciones definidos anteriormente
+	 * @return retorna la conecion a bbdd
+	 */
 	public static Connection getConnection() {
 		Connection conn = null;
 		String ps="";
-		
-		//Connectamos con el nombre sa y el password  
+		//intentara conectar, con las datos del DRIVER URL USUARIO Y PASSWORD
 		try {
 			Class.forName(DRIVER);
 			conn=DriverManager.getConnection(URL,"sa", ps);
 		}catch(ClassNotFoundException | SQLException e) {
 			String msn = "la exception es ";
-	       	 LOGGER.log(Level.INFO,msn, e);
+       	 LOGGER.log(Level.INFO,msn, e);
 		}
 		return conn;
 		
